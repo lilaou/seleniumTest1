0767f1f521f5b6941f40c1b13b2c170acafb84f7
Owen Li <lilaou@gmail.com>
2017-03-28 19:49:00 -0400

    chrome extensions loaded

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,10 +1,13 @@
 //import java.util.regex.Pattern;
 //import java.util.concurrent.TimeUnit;
+import java.io.File;
+
 import org.junit.*;
 //import static org.junit.Assert.*;
 //import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -13,11 +16,20 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class test1 {
   private WebDriver driver;
+	//private ChromeDriver driver;
   @Before
   public void setUp() throws Exception {
 	  //System.setProperty("webdriver.gecko.driver", "C:/Users/OwenLi/workspace/Library/geckodriver-v0.15.0-win64/geckodriver.exe");
+	  ChromeOptions options = new ChromeOptions ();
+
+	  options.addExtensions (new File("C:/Users/OwenLi/workspace/Library/WebAdvisor_v5.0.532.0.crx"));
+
+	  DesiredCapabilities capabilities = DesiredCapabilities.chrome();//new DesiredCapabilities ();
+
+	  capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 	  System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe");
-	  driver =new RemoteWebDriver(DesiredCapabilities.chrome());
+	   driver =new RemoteWebDriver(capabilities);
+	  //driver =new RemoteWebDriver(DesiredCapabilities.chrome());
 	  
   }
 // Search using keyword through Google search
@@ -32,10 +44,13 @@ public class test1 {
         //Click Search button
         driver.findElement(By.name("btnG")).click();
         Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".mcafee_HACKERSAFE")).click();
+        driver.get("chrome-extension://fheoggkfdfchfphceeifdbepaooicaho/html/annotationballoon.html");
+        //driver.findElement(By.cssSelector(".mcafee_HACKERSAFE"));
   }
 
   @After
   public void tearDown() throws Exception {
-    driver.quit();
+    //driver.quit();
     }
   }
